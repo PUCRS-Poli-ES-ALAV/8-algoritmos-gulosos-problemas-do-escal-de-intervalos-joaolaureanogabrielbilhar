34178aa34e208db96ee85dc351d94bfb421e91b7
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-05-24 15:17:30 -0300

    DONE

--- AlgoritmoGuloso.java
+++ AlgoritmoGuloso.java
@@ -10,43 +10,64 @@ public class AlgoritmoGuloso {
     public static class Intervalos implements Comparable<Intervalos> {
         public int maximo;
         public int minimo;
-        public List<Integer> lista;
 
         Intervalos(int minimo, int maximo) {
             if (minimo > maximo)
                 throw new IllegalArgumentException("Minimo maior que maximo");
             this.maximo = maximo;
             this.minimo = minimo;
-            this.lista = new ArrayList<Integer>();
-            for (int i = minimo; i <= maximo; i++)
-                this.lista.add(i);
         }
         public int compareTo(Intervalos toTest) {
             if (toTest.maximo == this.maximo && toTest.minimo == this.minimo)
                 return 0;
-            return (this.minimo > toTest.minimo) ? 1 : -1;
+            return (this.maximo > toTest.maximo) ? 1 : -1;
         }
 
         public String toString() {
             return "(" + this.minimo + ":" + this.maximo + ")";
         }
     }
-
-    public static Set sdm(int start, int last, int quantity) {
-        Set result = new HashSet<Intervalos>();
-        int i = start;
-        while(i);
-        return null;
-    }
-
     public static void main(String[] args) {
-        Intervalos[] intervalos = new Intervalos[] { new Intervalos(4, 8), new Intervalos(6, 7), new Intervalos(13, 14),
-                new Intervalos(4, 5), new Intervalos(2, 4), new Intervalos(6, 9), new Intervalos(7, 10),
-                new Intervalos(9, 11), new Intervalos(1, 6), new Intervalos(3, 13), new Intervalos(9, 12) };
-                Arrays.sort(intervalos);
-                for (Intervalos intervalos2 : intervalos) {
-                    System.out.println(intervalos2);
+        // TEST 1
+        ArrayList<Intervalos> intervalos = new ArrayList<Intervalos>();
+        Intervalos[] arrayIntervalos = new Intervalos[]{new Intervalos(4, 8), new Intervalos(6, 7), new Intervalos(13, 14),
+            new Intervalos(4, 5), new Intervalos(2, 4), new Intervalos(6, 9), new Intervalos(7, 10),
+            new Intervalos(9, 11), new Intervalos(1, 6), new Intervalos(3, 13), new Intervalos(9, 12) };
+                for (Intervalos intervalo : arrayIntervalos) {
+                    intervalos.add(intervalo);
                 }
+        System.out.println(AlgoritmoGuloso.sdm(new ArrayList(intervalos)));    
+
+
+        // TEST 2
+        intervalos = new ArrayList<Intervalos>();
+        arrayIntervalos = new Intervalos[]{
+            new Intervalos(0,4),new Intervalos(1,2),new Intervalos(2,4),new Intervalos(3,5),new Intervalos(3,6),
+            new Intervalos(5,6),new Intervalos(5,7),new Intervalos(6,7),new Intervalos(7,9),new Intervalos(8,10)
+        };
+                for (Intervalos intervalo : arrayIntervalos) {
+                    intervalos.add(intervalo);
+                }
+        System.out.println(AlgoritmoGuloso.sdm(new ArrayList(intervalos)));    
+        }
+    public static Set sdm(List<Intervalos> listaIntervalos) {
+        if(listaIntervalos == null || listaIntervalos.size() == 0)
+            return null;
+        if(listaIntervalos.size() == 1)
+        return new HashSet<Intervalos>(listaIntervalos);
+
+        Collections.sort(listaIntervalos);
+        Set result = new HashSet<Intervalos>();
+        int index = 0;
+        int lastMax = Integer.MIN_VALUE;
+        while(index < listaIntervalos.size()){
+            if(listaIntervalos.get(index).minimo >= lastMax){
+                result.add(listaIntervalos.get(index));
+                lastMax = listaIntervalos.get(index).maximo;
+            }
+        index++;
+        }
+        return result;
     }
 
 }
